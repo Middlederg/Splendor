cb26362caf0d83c42125f61494cc79e46e6a9ee1
JCL <middlederg@gmail.com>
2019-12-20 19:13:15 +0100

    Project reorganized

---
+++ src/Splendor.Forms/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Middlederg")]
+[assembly: AssemblyProduct("Splendor")]
+[assembly: AssemblyTitle("Splendor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("6a669bab-8a0c-4a12-b723-96dd1b937baa")]
+
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
---
+++ test/Splendor.Tests/GemasTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Splendor.Tests
+{
+    public class GemasTests
+    {
+        [Fact]
+        public void A_gem_should()
+        {
+
+        }
+    }
+}
