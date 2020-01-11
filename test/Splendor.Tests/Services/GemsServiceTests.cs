ecac047092fc572ecf6f794a5718627473e9d7bc
jcl86 <jorgecano86@gmail.com>
2020-01-11 22:22:45 +0100

    Test added

--- test/Splendor.Tests/Services/GemsServiceTests.cs
+++ test/Splendor.Tests/Services/GemsServiceTests.cs
@@ -1,12 +1,13 @@
 ﻿using Xunit;
 using Splendor.Domain;
+using FluentAssertions;
 
 namespace Splendor.Tests
 {
     [Collection(nameof(DefaultCollection))]
     public class GemsServiceTests
     {
-        private Game game;
+        private readonly Game game;
         public GemsServiceTests()
         {
             game = new Game(Prestige.FromScalar(15),
@@ -16,8 +17,17 @@ namespace Splendor.Tests
 
         public void ShouldAddGems()
         {
-            var service = new TakeGemsService(game.CurrentPlayer, game.Market, Gems.Onyx, Gems.Sapphire);
-            //service.CanBeTaken
+            var service = new TakeGemsService(game.CurrentPlayer,
+                game.Market, Gems.Onyx, Gems.Sapphire);
+            service.CanBeTaken().Should().BeTrue();
+            service.ShouldReturnGemsToMarket().Should().BeFalse();
+            service.GemsToReturnToMarket().Should().Be(0);
+            service.Take();
+            game.CurrentPlayer.Gems.Should().ContainEquivalentOf(Gems.Onyx);
+            game.CurrentPlayer.Gems.Should().ContainEquivalentOf(Gems.Sapphire);
+            game.CurrentPlayer.Gems.Count.Should().Be(2);
+            game.Market.AvaliableOfType(Gems.Onyx).Should().Be(3);
+            game.Market.AvaliableOfType(Gems.Sapphire).Should().Be(3);
         }
     }
 }
