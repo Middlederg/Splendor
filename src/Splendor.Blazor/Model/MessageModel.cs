cf26403fb7a84d4ab7a130c977610d3f6957128e
jcl86 <jorgecano86@gmail.com>
2021-02-01 20:26:56 +0100

    Playing

---
+++ src/Splendor.Blazor/Model/MessageModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Splendor.Blazor.Model
+{
+    public class MessageModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public void Set(string content, string title = null)
+        {
+            Title = title ?? "";
+            Content = content;
+        }
+
+        public bool IsEmpty() => string.IsNullOrWhiteSpace(Title) && string.IsNullOrWhiteSpace(Content);
+    }
+}
